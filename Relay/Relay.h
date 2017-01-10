a9f65803d578efc3a121490ed524a07c68d0f140
Evan Kimia <ekimia1987@gmail.com>
2017-01-09 23:56:45 -0800

    WIP multi platform.

--- Relay/Relay.h
+++ Relay/Relay.h
@@ -6,14 +6,10 @@
 //  Copyright © 2016 zero. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+@import Foundation;
 
 //! Project version number for Relay.
 FOUNDATION_EXPORT double RelayVersionNumber;
 
 //! Project version string for Relay.
 FOUNDATION_EXPORT const unsigned char RelayVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <Relay/PublicHeader.h>
-
-
