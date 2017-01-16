e30cff804ab243ba75028783f45500ddfb7dcb43
Evan Kimia <ekimia1987@gmail.com>
2017-01-15 17:39:04 -0800

    Disable the default submodule logic.

--- deploy.sh
+++ deploy.sh
@@ -28,7 +28,7 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]
 fi
 
 if [[ -z "${TRAVIS_TAG}" ]]; then
-	    echo "Skipping docs generation since we arent building from a tag."
+	echo "Skipping docs generation since we aren't building from a tag."
     doCompile
     exit 0
 fi
