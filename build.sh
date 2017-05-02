f127c10418c55ab1a9c4934c1b23b48604065eeb
Evan Kimia <evan@zerofinancial.com>
2017-05-02 13:27:51 -0700

    fdgdsfdsg'

--- build.sh
+++ build.sh
@@ -3,6 +3,10 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
 
+echo "--------------------------------"
+echo "Travis environmental variables:"
+printenv
+echo "--------------------------------"
 SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
 
