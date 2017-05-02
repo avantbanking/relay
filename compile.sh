e04748c68a509727640dcdb21e68594ab58a23e9
Evan Kimia <evan@zerofinancial.com>
2017-05-01 19:05:59 -0700

    update travis config.

--- compile.sh
+++ compile.sh
@@ -1,6 +1,4 @@
 echo "Compiling..."
 git submodule update --init --recursive
 carthage bootstrap
-xcodebuild test -project Relay.xcodeproj -scheme Relay -sdk iphonesimulator10.2 -destination 'platform=iOS Simulator,OS=10.2,id=22FA2149-1241-469C-BF6D-462D3837DB72' CODE_SIGNING_REQUIRED=NO
-
-xcodebuild test -project Relay.xcodeproj -scheme Relay -sdk macosx10.12 CODE_SIGNING_REQUIRED=NO
+fastlane scan
