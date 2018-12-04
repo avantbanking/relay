8a8557f4809a9e431fc5073c657cd276fc5d1c5d
Josh Wright <joshuawright11@gmail.com>
2018-12-03 16:47:24 -0800

    Update realm accessor to attempt a Realm nuke if things fail

--- Relay/Relay+Realm.swift
+++ Relay/Relay+Realm.swift
@@ -8,23 +8,48 @@
 
 import Foundation
 import RealmSwift
+import CocoaLumberjackSwift
+
 
 extension Relay {
     
+    
+    private var realmPath: URL {
+        return relayPath().appendingPathComponent(identifier + ".realm")
+    }
+    
+    
     /// Internal database, marked as internal for use when running tests.
     var realm: Realm {
-        let config = Realm.Configuration(fileURL: relayPath().appendingPathComponent(identifier + ".realm"),
+        let config = Realm.Configuration(fileURL: realmPath,
                                          schemaVersion: 1,
                                          migrationBlock: migrationHandler,
                                          objectTypes:[LogRecord.self])
         do {
             return try Realm(configuration: config)
         } catch {
-            fatalError("Error initializing Realm: \(error)")
+            // Nuke realm and try again
+            DDLogError("Relay hit an error loading realm! Nuking it.")
+            nukeRealm()
+            do {
+                return try Realm(configuration: config)
+            } catch {
+                fatalError("Relay hit an error loading realm, even after nuke!")
+            }
         }
     }
     
+    
     private func migrationHandler(_ migration: Migration, oldSchemaVersion: UInt64) {
         // 0 -> 1 did not require any migration code, but Realm demands a version bump
+    }
+    
+    
+    private func nukeRealm() {
+        do {
+            try FileManager.default.removeItem(at: realmPath)
+        } catch {
+            DDLogError("Error nuking Realm: \(error)")
+        }
     }
 }
